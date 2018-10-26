5f65829c555cdadd6d4f0668dfa231eaf9820f80
MF <vastolorde77@gmail.com>
2018-10-26 21:12:25 +0700

    Added Leak Detection

--- app/src/main/java/com/example/mf/pf2/BaseApplication.kt
+++ app/src/main/java/com/example/mf/pf2/BaseApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.mf.pf2.di.ActivityComponent
 import com.example.mf.pf2.di.AppModule
 import com.example.mf.pf2.di.DaggerActivityComponent
+import com.squareup.leakcanary.LeakCanary
 
 class BaseApplication : Application() {
 
@@ -13,7 +14,19 @@ class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        this.initializeDependencyInjection()
+        this.initializeLeakDetection()
+    }
+
+    fun initializeDependencyInjection(){
         activityComponent = DaggerActivityComponent.builder().appModule(AppModule(this)).build()
+    }
+
+    fun initializeLeakDetection(){
+        if (LeakCanary.isInAnalyzerProcess(this)){
+            return
+        }
+        LeakCanary.install(this)
     }
 
 }
